28a50d34664cfd8c9ff9d34777ccc637bead451f
JoseElias <jmuyulema9096@uta.edu.ec>
2023-06-27 07:53:48 -0500

    Estructura html de la pagina integrado

--- Paginas/CompletarReserva.php
+++ Paginas/CompletarReserva.php
@@ -15,3 +15,14 @@ $celPed = $_POST['cel_ped'];
 $mesa = $_POST['mesa'];
 $fecPed = $_POST['dia_ped'];
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
