8806ab4a06b5646484aed965c9ee53d406336efd
JoseElias <jmuyulema9096@uta.edu.ec>
2023-06-27 10:17:40 -0500

    php del header y footer incorporados en la pagina de CompletarReserva

--- Paginas/CompletarReserva.php
+++ Paginas/CompletarReserva.php
@@ -17,12 +17,19 @@ $fecPed = $_POST['dia_ped'];
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <?php require_once 'vistaHeader.html' ?>
+    <main>
+
+    </main>
+    <?php require_once 'vistaFooter.html' ?>
 </body>
+
 </html>
